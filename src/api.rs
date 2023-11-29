9b5188b3a498d130b45bc7dd71eb473d5d550e4c
Sosthene00 <674694@protonmail.ch>
2023-11-29 17:12:02 +0100

    api error management

--- src/api.rs
+++ src/api.rs
@@ -4,6 +4,7 @@ use bip39::Mnemonic;
 use bitcoin::{secp256k1::{Scalar, Secp256k1}, util::bip32::{DerivationPath, ExtendedPrivKey}, Network};
 use flutter_rust_bridge::StreamSink;
 
+use anyhow::Result;
 use crate::{
     constants::{LogEntry, ScanProgress, ScanStatus},
     wallet::{self, WalletMessage},
@@ -38,48 +39,56 @@ pub fn start_nakamoto() {
 }
 
 pub fn restart_nakamoto() {
-    nakamotoclient::restart_nakamoto_client().unwrap();
+    let res = nakamotoclient::restart_nakamoto_client();
+    if res.is_err() {
+        loginfo(&format!("{:?}", res.err()));
+    }
 }
 
-pub fn get_peer_count() -> u32 {
-    nakamotoclient::get_peer_count().unwrap()
+pub fn get_peer_count() -> Result<u32> {
+    nakamotoclient::get_peer_count()
 }
 
-pub fn scan_next_n_blocks(blob: String, n: u32) {
-    let mut wallet_msg: WalletMessage = serde_json::from_str(&blob).unwrap();
+pub fn scan_next_n_blocks(blob: String, n: u32) -> Result<()> {
+    let mut wallet_msg: WalletMessage = serde_json::from_str(&blob)?;
     let sp_receiver = &wallet_msg.wallet.sp_wallet;
 
-    let scan_key_path = DerivationPath::from_str(&format!("m/352'/{}/0'/1'/0", if sp_receiver.is_testnet { "1'" } else { "0'" })).unwrap();
+    let scan_key_path = DerivationPath::from_str(&format!("m/352'/{}/0'/1'/0", if sp_receiver.is_testnet { "1'" } else { "0'" }))?;
 
     let secp = Secp256k1::new();
     let network = if sp_receiver.is_testnet { Network::Signet } else { Network::Bitcoin };
-    let seed = Mnemonic::from_str(&wallet_msg.mnemonic).unwrap().to_seed("");
-    let scan_sk = ExtendedPrivKey::new_master(network, &seed).unwrap().derive_priv(&secp, &scan_key_path).unwrap().private_key;
+    let seed = Mnemonic::from_str(&wallet_msg.mnemonic)?.to_seed("");
+    let scan_sk = ExtendedPrivKey::new_master(network, &seed).unwrap().derive_priv(&secp, &scan_key_path)?.private_key;
 
-    let electrum_client = create_electrum_client().unwrap();
+    let electrum_client = create_electrum_client()?;
 
     let scan_key_scalar: Scalar = scan_sk.into();
 
-    nakamotoclient::scan_blocks(n, &mut wallet_msg.wallet, electrum_client, scan_key_scalar).unwrap();
+    let res = nakamotoclient::scan_blocks(n, &mut wallet_msg.wallet, electrum_client, scan_key_scalar);
+    if res.is_err() {
+        loginfo(&format!("{}", res.unwrap_err().to_string()));
+    }
+
+    Ok(())
 }
 
-pub fn scan_to_tip(blob: String) {
+pub fn scan_to_tip(blob: String) -> Result<()> {
     // 0 means scan to tip
-    scan_next_n_blocks(blob, 0);
+    scan_next_n_blocks(blob, 0)
 }
 
-pub fn get_wallet_info(blob: String) -> ScanStatus {
-    let wallet_msg: WalletMessage = serde_json::from_str(&blob).unwrap();
+pub fn get_wallet_info(blob: String) -> Result<ScanStatus> {
+    let wallet_msg: WalletMessage = serde_json::from_str(&blob)?;
 
     let scan_status = wallet_msg.wallet.scan_status;
 
     let scan_height = scan_status.scan_height;
-    let tip_height = nakamotoclient::get_tip().unwrap();
+    let tip_height = nakamotoclient::get_tip()?;
 
-    ScanStatus {
+    Ok(ScanStatus {
         scan_height,
         block_tip: tip_height,
-    }
+    })
 }
 
 pub fn get_wallet_balance(blob: String) -> u64 {
