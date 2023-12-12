d70c48e5b42ab2c903de203abea4de6d52da8a16
Sosthene00 <674694@protonmail.ch>
2023-12-12 14:50:06 +0100

    Add some const for psbt

--- src/constants.rs
+++ src/constants.rs
@@ -1,6 +1,13 @@
 use serde::{Serialize, Deserialize};
 use bitcoin::{secp256k1::PublicKey, OutPoint, Script, Txid};
 
+pub const PSBT_SP_PREFIX: &str = "sp";
+pub const PSBT_SP_SUBTYPE: u8 = 0;
+pub const PSBT_SP_TWEAK_KEY: &str = "tweak";
+pub const PSBT_SP_ADDRESS_KEY: &str = "address";
+
+pub const NUMS_KEY: &str = "50929b74c1a04954b78b4b6035e97a5e078a5a0f28ec96d547bfee9ace803ac0";
+
 pub struct LogEntry {
     // pub time_millis: i64,
     // pub level: i32,
@@ -40,8 +47,13 @@ pub struct OwnedOutput {
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
+pub(crate) struct SpendDestination {
+    pub address: String, // Can be either sp or regular address
+    pub value: u64
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct SpendMessage {
     pub inputs: Vec<OwnedOutput>,
-    pub outputs: Vec<String>,
-    pub fee: u32,
+    pub outputs: Vec<SpendDestination>,
 }
